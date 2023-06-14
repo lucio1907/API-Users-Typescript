259f6590f9dead1c497dbe12d318a2021f2e847a
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2023-06-14 12:16:41 -0300

    Api finished

--- src/app.ts
+++ src/app.ts
@@ -16,4 +16,4 @@ connectionMongoDB()
 app.use('/api/v1', routerLogin, routerUsers);
 
 const PORT = 8080 || process.env.PORT;
-app.listen(PORT, () => console.log("Server up!"));
+app.listen(PORT, "0.0.0.0", () => console.log("Server up!"));
